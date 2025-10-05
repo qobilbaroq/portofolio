72ab6563ddffb406255254f33fbb789b288b96d0
unknown <nabilmuhamad89@gmail.com>
2025-10-05 10:07:13 +0700

    add: responsif

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -2,20 +2,19 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center w-full px-8 py-6 z-50">
-      {/* Logo */}
-      <h1 className="text-foreground text-sm hover:text-text-muted-light cursor-pointer">
+    <nav className="flex justify-between items-center w-full px-4 md:px-8 py-6 z-50">
+      <h1 className="hidden md:block text-foreground text-sm hover:text-text-muted-light cursor-pointer">
         BAROQ
       </h1>
 
-      {/* Menu */}
-      <div className="flex justify-between items-center bg-navbar w-[400px] py-3 px-6 rounded-lg cursor-pointer">
+      <div className="flex justify-between items-center bg-navbar md:py-3 py-4 px-6 rounded-lg cursor-pointer w-full md:w-[400px]">
         <h1 className="text-foreground text-sm">MENU</h1>
         <div className="text-foreground text-sm">icon</div>
       </div>
 
-      {/* Contact */}
-      <h1 className="text-foreground text-sm cursor-pointer">CONTACT</h1>
+      <h1 className="hidden md:block text-foreground text-sm hover:text-text-muted-light cursor-pointer">
+        CONTACT
+      </h1>
     </nav>
   );
 };
